a94545cfe47384d8289766a3eccb37ca65836019
jervw <vuolajere@gmail.com>
2022-12-25 17:36:39 +0200

    remove unused wallpapers

--- .wallpaper/single/main.py
+++
@@ -1,6 +0,0 @@
-from ImageGoNord import NordPaletteFile, GoNord
-
-# E.g. Replace pixel by pixel
-go_nord = GoNord()
-image = go_nord.open_image("jinx.jpg")
-go_nord.convert_image(image, save_path='jinx-nord.jpg')
