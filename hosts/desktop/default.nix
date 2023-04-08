8865a911bf257a91c3a867bc54c3ea090a4748a8
jervw <vuolajere@gmail.com>
2023-04-08 17:01:02 +0300

    add xrandr session command

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -38,8 +38,11 @@
       xkbOptions = "grp:win_space_toggle";
 
       desktopManager.xterm.enable = false;
-      displayManager.defaultSession = "none+i3";
 
+      displayManager = {
+        defaultSession = "none+i3";
+        sessionCommands = "${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate right --output DP-0 --primary --mode 2560x1440 --rate 143.97 --pos 1080x106 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off";
+      };
 
       windowManager.i3 = {
         enable = true;
