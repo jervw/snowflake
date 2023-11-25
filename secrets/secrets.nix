46f28ba0961920c0bd0d3de53a2c05908a78c89e
jervw <vuolajere@gmail.com>
2023-11-25 17:01:04 +0200

    add another private key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   jervw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINfHThh8zEBkr3RTpg0xoFrrL/JQ2VUkXQo/MNYXxsUI";
   loki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk7bYxT0obpeDc9w/6n1pUyRv0G/3irfTnutOneH4yb";
+  thor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEExgGsdOkKL/txMJE89lrF0mROn5EVZEGtAPA1WHyXF";
 in {
-  "pia.age".publicKeys = [jervw loki];
+  "pia.age".publicKeys = [jervw loki thor];
 }
