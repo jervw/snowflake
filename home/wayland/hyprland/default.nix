88bb0955474394d4ee60aefd4ad3e2cd398b87a2
jervw <vuolajere@gmail.com>
2024-03-04 15:22:19 +0200

    fix formatting

--- home/wayland/hyprland/default.nix
+++ home/wayland/hyprland/default.nix
@@ -1,20 +1,6 @@
-_:let
+_: let
   mod = "SUPER";
   modshift = "${mod}SHIFT";
-
-  # binds $mod + [shift +] {1..5} to [move to] workspace {1..5}
-  workspaces = builtins.concatLists (builtins.genList (
-      x: let
-        ws = let
-          c = (x + 1) / 5;
-        in
-          builtins.toString (x + 1 - (c * 5));
-      in [
-        "${mod}, ${ws}, workspace, ${toString (x + 1)}"
-        "${mod} SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-      ]
-    )
-    5);
 in {
   wayland.windowManager.hyprland = {
     enable = true;
