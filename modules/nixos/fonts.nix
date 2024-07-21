d45e912d1f6468d55c673a8e1ddf89c093ce63cf
jervw <jervw@pm.me>
2024-07-21 20:04:49 +0300

    remove pkg: apple-fonts

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -7,7 +7,6 @@
       noto-fonts-emoji
       font-awesome
       corefonts
-      (callPackage ../../pkgs/apple-fonts {})
       (nerdfonts.override {fonts = ["JetBrainsMono"];})
       (google-fonts.override {fonts = ["Inter"];})
     ];
