aca7dc380893ef45ba45ae314d02b7657b024fee
jervw <jervw@pm.me>
2024-07-23 09:35:25 +0300

    fastfetch rice

--- home/core/fastfetch.nix
+++ home/core/fastfetch.nix
@@ -4,30 +4,83 @@ _: {
 
     settings = {
       logo = {
-        source = "nixos_small";
-        padding = {
-          right = 1;
-        };
+        type = "small";
       };
+
       display = {
-        binaryPrefix = "si";
-        color = "blue";
-        separator = "  ";
+        separator = "";
+        keyWidth = 12;
       };
+
       modules = [
         {
-          type = "datetime";
-          key = "Date";
-          format = "{1}-{3}-{11}";
+          key = " user";
+          type = "title";
+          format = "{1}@{2}";
+          keyColor = "31";
         }
         {
-          type = "datetime";
-          key = "Time";
-          format = "{14}:{17}:{20}";
+          key = "󰅐 up";
+          type = "uptime";
+          keyColor = "33";
         }
-        "break"
-        "player"
-        "media"
+        {
+          key = "󰟾 distro";
+          type = "os";
+          keyColor = "34";
+          format = "{3}";
+        }
+        {
+          key = " kernel";
+          type = "kernel";
+          keyColor = "35";
+        }
+        {
+          key = "󰇄 desktop";
+          type = "de";
+          keyColor = "36";
+        }
+        {
+          key = " term";
+          type = "terminal";
+          keyColor = "31";
+          format = "{1}";
+        }
+        {
+          key = " shell";
+          type = "shell";
+          keyColor = "32";
+          format = "{1}";
+        }
+        {
+          key = "󰍛 cpu";
+          type = "cpu";
+          showPeCoreCount = true;
+          keyColor = "33";
+        }
+        {
+          key = "󰉉 disk";
+          type = "disk";
+          folders = "/";
+          keyColor = "34";
+        }
+        {
+          key = " memory";
+          type = "memory";
+          keyColor = "35";
+        }
+        {
+          key = "󰩟 network";
+          type = "localip";
+          format = "{1} ({4})";
+          keyColor = "36";
+        }
+        {
+          key = " colors";
+          type = "colors";
+          symbol = "circle";
+          keyColor = "39";
+        }
       ];
     };
   };
