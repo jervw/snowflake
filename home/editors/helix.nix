5798a5b2a827d4c05df2197dc1640d2c958f7739
jervw <jervw@pm.me>
2024-07-26 06:15:22 +0300

    Update helix LSP language configuration

--- home/editors/helix.nix
+++ home/editors/helix.nix
@@ -9,12 +9,12 @@
     package = inputs.helix.packages."x86_64-linux".default;
     defaultEditor = true;
 
+    # Zero-conf packages
     extraPackages = with pkgs; [
       nodePackages.bash-language-server
-      nodePackages.typescript-language-server
-      marksman
-      clang-tools
-      rust-analyzer
+      gopls
+      markdown-oxide
+      nil
     ];
 
     settings = {
@@ -27,6 +27,7 @@
         true-color = true;
         undercurl = true;
         smart-tab.enable = false;
+        lsp.display-inlay-hints = true;
         end-of-line-diagnostics = "hint";
         inline-diagnostics.cursor-line = "warning";
         whitespace.characters = {
@@ -41,11 +42,6 @@
         statusline = {
           left = ["mode" "spacer" "version-control" "file-name" "file-modification-indicator"];
           right = ["spinner" "diagnostics" "file-encoding" "file-type" "position"];
-          mode = {
-            normal = "NORMAL";
-            insert = "INSERT";
-            select = "SELECT";
-          };
         };
         indent-guides.render = true;
         gutters.line-numbers.min-width = 1;
@@ -80,34 +76,68 @@
         {
           name = "rust";
           auto-format = true;
-          formatter = {
-            command = lib.getExe rustfmt;
-          };
+          formatter.command = lib.getExe rustfmt;
+        }
+        {
+          name = "c";
+          auto-format = true;
+          formatter.command = "${pkgs.clang-tools}/bin/clangd-tidy";
+        }
+        {
+          name = "python";
+          language-servers = ["pyright" "ruff"];
+          auto-format = true;
         }
       ];
-      language-server = with pkgs; {
-        nil = {
-          command = lib.getExe nil;
-        };
-        marksman = {
-          command = lib.getExe marksman;
+
+      # LSP's
+      language-server = {
+        clangd = {
+          command = "${pkgs.clang-tools}/bin/clangd";
+          clangd.fallbackFlags = ["-std=c++2b"];
         };
         rust-analyzer = {
+          command = "${pkgs.rust-analyzer}/bin/rust-analyzer";
           timeout = 120;
           config.check = {
             command = "clippy";
           };
         };
+        pyright = {
+          command = "${pkgs.pyright}/bin/pyright-langserver";
+          args = ["--stdio"];
+          config = {
+            reportUnusedImport = true;
+            reportMissingTypeStubs = false;
+            python.analysis = {
+              typeCheckingMode = "basic";
+              autoImportCompletions = true;
+            };
+          };
+        };
+        ruff = {
+          command = "${pkgs.ruff}/bin/ruff";
+          args = ["server"];
+        };
+        vscode-html-language-server = {
+          command = "${pkgs.nodePackages.vscode-langservers-extracted}/bin/vscode-html-language-server";
+          args = ["--stdio"];
+        };
+        vscode-json-language-server = {
+          command = "${pkgs.nodePackages.vscode-langservers-extracted}/bin/vscode-json-language-server";
+          args = ["--stdio"];
+        };
+        vscode-css-language-server = {
+          command = "${pkgs.nodePackages.vscode-langservers-extracted}/bin/vscode-css-language-server";
+          args = ["--stdio"];
+        };
       };
     };
   };
 
-  home = {
-    # make background transparent
-    file.".config/helix/themes/zed_onedark_transparent.toml".text = ''
-      inherits = "zed_onedark"
-      "ui.background" = {}
-      "ui.virtual.jump-label" = { fg = "yellow", modifiers = ["bold"] }
-    '';
-  };
+  home.file.".config/helix/themes/zed_onedark_transparent.toml".text = ''
+    inherits = "zed_onedark"
+    "ui.background" = {}
+    "ui.virtual.jump-label" = { fg = "yellow", bg = "faint-gray", modifiers = ["bold" ] }
+  '';
 }
