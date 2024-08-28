58c53047cb5b453940bea57a698d3b295ae319ad
jervw <jervw@pm.me>
2024-08-28 21:48:23 +0300

    Set default terminal for xdg-open

--- modules/nixos/wayland.nix
+++ modules/nixos/wayland.nix
@@ -23,12 +23,22 @@
     ];
   };
 
-  xdg.portal = {
-    enable = true;
-    config.common.default = "*";
-    xdgOpenUsePortal = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
+  xdg = {
+    portal = {
+      enable = true;
+      config.common.default = "*";
+      xdgOpenUsePortal = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
+    };
+    terminal-exec = {
+      enable = true;
+      settings = {
+        default = [
+          "org.codeberg.dnkl.foot.desktop"
+        ];
+      };
+    };
   };
 }
