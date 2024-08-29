93c81b5f446b7049a38f4cc1196b857e2a89b55d
jervw <jervw@pm.me>
2024-08-29 09:37:26 +0300

    Convert wallpapers to png

---
+++ home/desktop/mpv.nix
@@ -0,0 +1,15 @@
+_: {
+  programs.mpv = {
+    enable = true;
+    bindings = {
+      "ALT+k" = "add sub-scale +0.1";
+      "ALT+j" = "add sub-scale -0.1";
+      "ALT+=" = "add video-zoom +0.1";
+      "ALT+-" = "add video-zoom -0.1";
+    };
+    config = {
+      gpu-context = "wayland";
+      save-position-on-quit = true;
+    };
+  };
+}
