fc2a85ab041c129d9e36c2e9bfcd9c5571929b0d
jervw <jervw@pm.me>
2024-09-01 11:35:00 +0300

    Small changes to gamemode start script

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -7,17 +7,17 @@
   gamemodeStart = pkgs.writeShellScript "gamemode-start-script" ''
     ${pkgs.libnotify}/bin/notify-send 'GameMode started'
     ${hyprctl} --instance 0 --batch "\
-        keyword monitor HDMI-A-1, disable;\
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur:enabled 0;\
         keyword general:border_size 1;\
+        keyword monitor HDMI-A-1, disable;\
         keyword decoration:rounding 0"
   '';
 
   gamemodeEnd = pkgs.writeShellScript "gamemode-end-script" ''
     ${pkgs.libnotify}/bin/notify-send 'GameMode stopped'
-    ${hyprctl} reload
+    ${hyprctl} --instance 0 reload
   '';
 in {
   programs.steam = {
