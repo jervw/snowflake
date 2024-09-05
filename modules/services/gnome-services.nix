b4110dad199dfb3c04c27649c316f75960901cb7
jervw <jervw@pm.me>
2024-09-05 16:08:07 +0300

    gnome-settings-daemon rename

--- modules/services/gnome-services.nix
+++ modules/services/gnome-services.nix
@@ -2,7 +2,7 @@
   services = {
     dbus.packages = with pkgs; [
       gcr
-      gnome.gnome-settings-daemon
+      gnome-settings-daemon
     ];
     gnome.gnome-keyring.enable = true;
     gvfs.enable = true;
