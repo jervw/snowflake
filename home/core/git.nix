41a2bedbc84b24da5c295fcd283ef4d9a198831b
jervw <jervw@pm.me>
2024-09-09 15:49:35 +0300

    Add GitHub CLI configuration to git.nix module

--- home/core/git.nix
+++ home/core/git.nix
@@ -1,27 +1,42 @@
-{
-  programs.git = {
-    enable = true;
-    userName = "jervw";
-    userEmail = "jervw@pm.me";
-
-    ignores = [
-      ".direnv/"
-      ".env"
-      ".envrc"
-    ];
-
-    extraConfig = {
-      init = {defaultBranch = "main";};
-      push.autoSetupRemote = true;
-    };
-
-    lfs = {
+{pkgs, ...}: {
+  programs = {
+    git = {
       enable = true;
-    };
+      userName = "jervw";
+      userEmail = "jervw@pm.me";
 
-    signing = {
-      key = "56C25B5B20756352B4B0E17EF188371747DA5895";
-      signByDefault = true;
+      ignores = [
+        ".direnv/"
+        ".env"
+        ".envrc"
+      ];
+
+      extraConfig = {
+        init = {defaultBranch = "main";};
+        push.autoSetupRemote = true;
+      };
+
+      lfs = {
+        enable = true;
+      };
+
+      signing = {
+        key = "56C25B5B20756352B4B0E17EF188371747DA5895";
+        signByDefault = true;
+      };
+    };
+    gh = {
+      enable = true;
+      settings = {
+        editor = "hx";
+        prompt = "enabled";
+      };
+      extensions = with pkgs; [
+        gh-eco
+        gh-markdown-preview
+        gh-copilot
+        gh-dash
+      ];
     };
   };
 }
