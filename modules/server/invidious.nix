5bb18721d66de7d32e641df63db34dfa10054246
jervw <jervw@pm.me>
2024-09-26 04:34:40 +0300

    Add invidious reverse proxy and settings

--- modules/server/invidious.nix
+++ modules/server/invidious.nix
@@ -1,6 +1,19 @@
 _: {
+  services.caddy.virtualHosts."iv.jervw.dev".extraConfig = ''
+    reverse_proxy http://thor:3011
+    import cloudflare
+  '';
+
   services.invidious = {
     enable = true;
     port = 3011;
+    settings = {
+      registration_enabled = false;
+      captcha_enabled = false;
+      region = "FI";
+      quality = "dash";
+      quality_dash = "1440p";
+      save_player_pos = true;
+    };
   };
 }
