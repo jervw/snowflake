a76e9c29b3caa69191bed5b79d1ac22f500aaa92
jervw <jervw@pm.me>
2024-09-27 02:56:52 +0300

    Add fenrir to secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
-  jervw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDbHGOAvDf6oXvhsHFiSQjn8m0tSP7JP2XJUNI2JDnm ";
+  jervw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDbHGOAvDf6oXvhsHFiSQjn8m0tSP7JP2XJUNI2JDnm";
   loki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7Xm5sE+PbatmSLaX5PT89S956tclHqvc9NUuzzCmeF";
   thor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM/pd5zQYAXuve8QVj6947m/OTfj3O36Czx6/GUpD1BN";
-  systems = [loki thor];
+  fenrir = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOAj0+XMWCCv/FyM8Y3qh9yfkXn83l+MI7CKeXCSm2d";
+  systems = [loki thor fenrir];
 in {
   "cloudflare.age".publicKeys = [jervw] ++ systems;
 }
