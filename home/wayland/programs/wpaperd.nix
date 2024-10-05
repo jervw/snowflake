a19b4854d988136b465eec6e5f7ec02cd82c4afb
jervw <jervw@pm.me>
2024-10-05 08:18:37 +0300

    feat(wpaperd): Random wallpaper groups, override src to master branch

--- home/wayland/programs/wpaperd.nix
+++ home/wayland/programs/wpaperd.nix
@@ -1,13 +1,34 @@
-{self, ...}: {
+{
+  self,
+  pkgs,
+  lib,
+  ...
+}: let
+  version = "git";
+  pname = "wpaperd-${version}";
+  src = pkgs.fetchFromGitHub {
+    owner = "danyspin97";
+    repo = "wpaperd";
+    rev = "62af4392f3e447592f768f5420821a344d190107";
+    hash = "sha256-6ThcLPPfdEDtAEX91WIa6zf8piPIqRvdG68+m3JWXvM=";
+  };
+in {
   programs.wpaperd = {
     enable = true;
+    package = pkgs.wpaperd.overrideAttrs (oldAttrs: {
+      inherit version pname src;
+      cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
+        name = "${pname}-vendor.tar.gz";
+        inherit src;
+        outputHash = "sha256-pRV6WexQ5N0gmkQkDo2h7O42K8DkOch43BAd3ffBe5A=";
+      });
+    });
     settings = {
-      default = {
-        duration = "30m";
-        mode = "center";
-      };
       any = {
         path = "${self}/wallpapers";
+      };
+      default.sorting.groupedrandom = {
+        group = 1;
       };
     };
   };
