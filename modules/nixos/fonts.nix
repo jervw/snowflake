d97f0d059e0528c6246741b0f8fbaaa75b6961f9
jervw <jervw@pm.me>
2024-10-23 12:06:50 +0300

    fix: Updated noto-fonts-cfk package name

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -3,7 +3,7 @@
     enableDefaultPackages = false;
     packages = with pkgs; [
       noto-fonts
-      noto-fonts-cjk
+      noto-fonts-cjk-sans
       noto-fonts-emoji
       font-awesome
       corefonts
