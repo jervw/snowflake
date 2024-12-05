5f9ef684b60fcf259009eab5e19c3cde90cb2384
jervw <jervw@pm.me>
2024-12-06 01:55:53 +0200

    zellij: Update theme to onedark

--- home/core/zellij.nix
+++ home/core/zellij.nix
@@ -2,25 +2,8 @@ _: {
   programs.zellij = {
     enable = true;
     settings = {
-      theme = "nightfox";
-      simplified_ui = true;
+      theme = "onedark";
       pane_frames = false;
-
-      themes = {
-        nightfox = {
-          fg = "#cdcecf";
-          bg = "#131a24";
-          black = "#393b44";
-          red = "#c94f6d";
-          green = "#81b29a";
-          yellow = "#dbc074";
-          blue = "#719cd6";
-          magenta = "#9d79d6";
-          cyan = "#63cdcf";
-          white = "#dfdfe0";
-          orange = "#f4a261";
-        };
-      };
     };
   };
 }
