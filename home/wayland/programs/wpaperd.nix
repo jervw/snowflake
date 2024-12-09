e91aa0e2f0f8221eed0cbae12ac340860ad65ea0
jervw <jervw@pm.me>
2024-12-09 16:29:40 +0200

    wpaperd: Use nixpkgs package

--- home/wayland/programs/wpaperd.nix
+++ home/wayland/programs/wpaperd.nix
@@ -1,28 +1,6 @@
-{
-  self,
-  pkgs,
-  lib,
-  ...
-}: let
-  version = "git";
-  pname = "wpaperd-${version}";
-  src = pkgs.fetchFromGitHub {
-    owner = "danyspin97";
-    repo = "wpaperd";
-    rev = "3980d9ed99f8f61a8831da332b9dae8f513c2417";
-    hash = "sha256-5/cfaeNVkJOf1X76yOIZxHE+Uk1mEVgwMMYAlBIXGYI=";
-  };
-in {
+{self, ...}: {
   programs.wpaperd = {
     enable = true;
-    package = pkgs.wpaperd.overrideAttrs (oldAttrs: {
-      inherit version pname src;
-      cargoDeps = oldAttrs.cargoDeps.overrideAttrs (lib.const {
-        name = "${pname}-vendor.tar.gz";
-        inherit src;
-        outputHash = "sha256-8IsMgNgFTUobTTr816VIp7hQ4RySMBTnpIjfiABHagc=";
-      });
-    });
     settings = {
       default = {
         path = "${self}/wallpapers";
