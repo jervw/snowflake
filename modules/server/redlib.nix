124437f5de6f637a3c92bd1a0b62d4e9cdc0318f
jervw <jervw@pm.me>
2024-12-19 22:44:10 +0200

    Add redlib settings

--- modules/server/redlib.nix
+++ modules/server/redlib.nix
@@ -1,4 +1,8 @@
-_: {
+{
+  self,
+  config,
+  ...
+}: {
   services.caddy.virtualHosts."redlib.jervw.dev".extraConfig = ''
     reverse_proxy http://thor:8081
     import cloudflare
@@ -6,6 +10,21 @@ _: {
 
   services.redlib = {
     enable = true;
+    settings = {
+      REDLIB_DEFAULT_WIDE = "on";
+      REDLIB_DEFAULT_SHOW_NSFW = "on";
+      REDLIB_DEFAULT_USE_HLS = "on";
+      REDLIB_DEFAULT_HIDE_HLS_NOTIFICATION = "on";
+    };
     port = 8081;
+  };
+
+  age.secrets.reddit.file = "${self}/secrets/reddit.age";
+
+  # Used for REDLIB_DEFAULT_SUBSCRIPTIONS, i prefer to keep my subs private
+  systemd.services.redlib = {
+    serviceConfig = {
+      EnvironmentFile = [config.age.secrets.reddit.path];
+    };
   };
 }
