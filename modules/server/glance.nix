0f93cdf8410df5f73e14fb8201a3a4e2b18e0e74
jervw <jervw@pm.me>
2024-12-28 23:40:04 +0200

    glance: Initial config

--- modules/server/glance.nix
+++ modules/server/glance.nix
@@ -4,12 +4,70 @@
     import cloudflare
   '';
 
-  services.glances = {
+  services.glance = {
     enable = true;
     openFirewall = true;
     settings = {
-      host = "0.0.0.0";
-      port = 5678;
+      pages = [
+        {
+          name = "Startpage";
+          width = "slim";
+          hideDesktopNavigation = true;
+          centerVertically = true;
+          columns = [
+            {
+              size = "full";
+              widgets = [
+                {
+                  type = "search";
+                  search-engine = "https://kagi.com/search?q={QUERY}";
+                  autofocus = true;
+                }
+                {
+                  type = "bookmarks";
+                  groups = [
+                    {
+                      title = "General";
+                      links = [
+                        {
+                          title = "Proton Mail";
+                          url = "https://mail.proton.me/u/0/";
+                        }
+                        {
+                          title = "Github";
+                          url = "https://github.com/";
+                        }
+                      ];
+                    }
+                    {
+                      title = "Entertainment";
+                      links = [
+                        {
+                          title = "YouTube";
+                          url = "https://www.youtube.com/";
+                        }
+                      ];
+                    }
+                    {
+                      title = "Social";
+                      links = [
+                        {
+                          title = "Reddit";
+                          url = "https://www.redlib.jervw.dev/";
+                        }
+                      ];
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        }
+      ];
+      server = {
+        host = "0.0.0.0";
+        port = 5678;
+      };
     };
   };
 }
