b59bf528b1f257da83ad54b5cbd60083722d0ab3
jervw <jervw@pm.me>
2025-01-05 02:08:54 +0200

    xdg: Add only specific xdg-portals

--- modules/nixos/wayland.nix
+++ modules/nixos/wayland.nix
@@ -25,8 +25,11 @@
   xdg = {
     portal = {
       enable = true;
-      config.common.default = "*";
       xdgOpenUsePortal = true;
+      config = {
+        common.default = ["gtk"];
+        hyprland.default = ["gtk" "hyprland"];
+      };
       extraPortals = with pkgs; [
         xdg-desktop-portal-gtk
       ];
