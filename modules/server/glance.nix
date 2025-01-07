0f43a185d5a475e7501edf297a5490ec756ca063
jervw <jervw@pm.me>
2025-01-08 01:18:09 +0200

    Add glance widgets

--- modules/server/glance.nix
+++ modules/server/glance.nix
@@ -8,13 +8,96 @@
     enable = true;
     openFirewall = true;
     settings = {
+      server = {
+        host = "0.0.0.0";
+        port = 5678;
+      };
+      branding = {
+        logo-text = "DASH";
+        favicon-url = "https://jervw.dev/favicon-7a187dc8cc543a31.png";
+        hide-footer = true;
+      };
       pages = [
         {
-          name = "Startpage";
-          width = "slim";
-          hideDesktopNavigation = true;
-          centerVertically = true;
+          name = "Home";
           columns = [
+            {
+              size = "small";
+              widgets = [
+                {
+                  type = "calendar";
+                }
+                {
+                  type = "bookmarks";
+                  groups = [
+                    {
+                      links = [
+                        {
+                          title = "Plex";
+                          icon = "si:plex";
+                          url = "https://plex.jervw.dev";
+                        }
+                        {
+                          title = "Jellyfin";
+                          icon = "si:jellyfin";
+                          url = "https://watch.jervw.dev";
+                        }
+                        {
+                          title = "Immich";
+                          icon = "si:immich";
+                          url = "https://media.jervw.dev";
+                        }
+                        {
+                          title = "Audiobookshelf";
+                          icon = "si:audiobookshelf";
+                          url = "https://shelf.jervw.dev";
+                        }
+                        {
+                          title = "Sonarr";
+                          icon = "si:sonarr";
+                          url = "https://sonarr.jervw.dev";
+                        }
+                        {
+                          title = "Radarr";
+                          icon = "si:radarr";
+                          url = "https://radarr.jervw.dev";
+                        }
+                        {
+                          title = "qBittorrent";
+                          icon = "si:qbittorrent";
+                          url = "https://dl.jervw.dev";
+                        }
+                        {
+                          title = "AdGuard Home";
+                          icon = "si:adguard";
+                          url = "https://dns.jervw.dev";
+                        }
+                        {
+                          title = "Redlib";
+                          icon = "si:reddit";
+                          url = "https://redlib.jervw.dev";
+                        }
+                      ];
+                    }
+                  ];
+                }
+                {
+                  type = "twitch-channels";
+                  collapse-after = 10;
+                  channels = [
+                    "xqc"
+                    "theprimeagen"
+                    "loltyler1"
+                    "sodapoppin"
+                    "erobb221"
+                    "caseoh_"
+                    "Mizkif"
+                    "Emiru"
+                    "KaiCenat"
+                  ];
+                }
+              ];
+            }
             {
               size = "full";
               widgets = [
@@ -24,38 +107,58 @@
                   autofocus = true;
                 }
                 {
-                  type = "bookmarks";
-                  groups = [
+                  type = "group";
+                  widgets = [
                     {
-                      title = "General";
-                      links = [
-                        {
-                          title = "Proton Mail";
-                          url = "https://mail.proton.me/u/0/";
-                        }
-                        {
-                          title = "Github";
-                          url = "https://github.com/";
-                        }
-                      ];
+                      type = "reddit";
+                      show-thumbnails = true;
+                      subreddit = "worldnews";
                     }
                     {
-                      title = "Entertainment";
-                      links = [
-                        {
-                          title = "YouTube";
-                          url = "https://www.youtube.com/";
-                        }
-                      ];
+                      type = "reddit";
+                      show-thumbnails = true;
+                      subreddit = "linux";
                     }
                     {
-                      title = "Social";
-                      links = [
-                        {
-                          title = "Reddit";
-                          url = "https://www.redlib.jervw.dev/";
-                        }
-                      ];
+                      type = "reddit";
+                      show-thumbnails = true;
+                      subreddit = "NixOS";
+                    }
+                  ];
+                }
+                {
+                  type = "rss";
+                  style = "horizontal-cards";
+                  limit = 10;
+                  collapse-after = 10;
+                  cache = "1h";
+                  feeds = [
+                    {
+                      url = "https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss";
+                      title = "Yle";
+                    }
+                  ];
+                }
+                {
+                  type = "hacker-news";
+                }
+              ];
+            }
+            {
+              size = "small";
+              widgets = [
+                {
+                  type = "weather";
+                  location = "Espoo, Finland";
+                }
+                {
+                  type = "rss";
+                  limit = 10;
+                  cache = "1h";
+                  feeds = [
+                    {
+                      url = "https://www.phoronix.com/rss.php";
+                      title = "Phoronix";
                     }
                   ];
                 }
@@ -64,10 +167,6 @@
           ];
         }
       ];
-      server = {
-        host = "0.0.0.0";
-        port = 5678;
-      };
     };
   };
 }
