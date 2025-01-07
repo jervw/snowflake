4d318b6c4068f5e39f47cfed781a39c3ebf5129c
jervw <jervw@pm.me>
2025-01-07 02:02:20 +0200

    mpv: Cleanup module

--- home/desktop/mpv.nix
+++ home/desktop/mpv.nix
@@ -1,12 +1,4 @@
 {pkgs, ...}: {
-  services.plex-mpv-shim = {
-    enable = true;
-    settings = {
-      mpv_ext = true;
-      mpv_ext_no_ovr = true;
-      fullscreen = false;
-    };
-  };
   programs.mpv = {
     enable = true;
     bindings = {
@@ -15,12 +7,17 @@
       "ALT+=" = "add video-zoom +0.1";
       "ALT+-" = "add video-zoom -0.1";
     };
-    config = {
-      ytdl-format = "bestvideo+bestaudio";
-    };
     scripts = with pkgs.mpvScripts; [
       uosc # UI
       acompressor # ffmpeg audio compress
     ];
+  };
+  services.plex-mpv-shim = {
+    enable = true;
+    settings = {
+      mpv_ext = true;
+      mpv_ext_no_ovr = true;
+      fullscreen = false;
+    };
   };
 }
