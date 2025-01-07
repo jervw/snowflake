ca0a2db2c794192b48a538a6b2d0820829e07064
jervw <jervw@pm.me>
2025-01-08 01:17:01 +0200

    loki: Enable zram swap

--- hosts/loki/hardware-configuration.nix
+++ hosts/loki/hardware-configuration.nix
@@ -38,7 +38,10 @@
     };
   };
 
-  swapDevices = [];
+  zramSwap = {
+    enable = true;
+    algorithm = "lz4";
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
