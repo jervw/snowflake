a21430ad0a13acb6a543c9e4334561fe82bf1604
jervw <jervw@pm.me>
2025-01-13 00:34:02 +0200

    nix: Remove unused substituters

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -51,9 +51,6 @@
       trusted-users = ["root" "@wheel"];
       substituters = [
         "https://cache.nixos.org?priority=10"
-        "https://anyrun.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://helix.cachix.org"
         "https://nyx.chaotic.cx"
         "https://cache.soopy.moe"
       ];
@@ -61,10 +58,7 @@
         "https://cache.soopy.moe"
       ];
       trusted-public-keys = [
-        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo="
       ];
