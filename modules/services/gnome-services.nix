9eabdfc8c6a1d3dc799d3be5de59b53238f96a04
jervw <jervw@pm.me>
2025-01-17 03:37:53 +0200

    Remove ugly hack

--- modules/services/gnome-services.nix
+++ modules/services/gnome-services.nix
@@ -1,14 +1,11 @@
 {pkgs, ...}: {
   services = {
     dbus.packages = with pkgs; [
-      # gnome-settings-daemon
+      gnome-settings-daemon
     ];
     gnome.gnome-keyring.enable = true;
     gvfs.enable = true;
   };
 
   programs.seahorse.enable = true;
-
-  # FIX very haram, trying to troubleshoot why keyring doesnt unlock on login
-  security.pam.services."*".enableGnomeKeyring = true;
 }
