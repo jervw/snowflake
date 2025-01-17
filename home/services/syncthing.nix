4d91ec6f540418032bec44dd299dd72c0cb692cb
jervw <jervw@pm.me>
2025-01-17 20:46:22 +0200

    home: Cleanup syncthing module and add phone device

--- home/services/syncthing.nix
+++ home/services/syncthing.nix
@@ -1,25 +1,29 @@
-_: let
-  devices = ["loki" "thor"];
-in {
+{config, ...}: {
   services.syncthing = {
     enable = true;
     guiAddress = "0.0.0.0:8384";
     settings = {
       devices = {
+        # Desktop
         loki = {
           id = "5JQOQEZ-EW42SJV-VZOA4TN-LAPYPYY-QDSRR6T-DTP3BC6-ZQGYTIH-YTTPVQQ";
           autoAcceptFolders = true;
         };
+        # Server
         thor = {
           id = "J4JVFCR-ND6TGKN-AKRJ3LA-XUYAYLI-RPQBZO4-OWLILPZ-2LFJH5V-5VWNOQM";
           autoAcceptFolders = true;
+        };
+        # Phone
+        tyr = {
+          id = "LF4JYTN-K6OF2N4-JTVUKTU-F5W65QQ-KBAZGXU-46HWVEO-P3VYVGY-SXYKMAM";
         };
       };
 
       folders = {
         "~/sync" = {
           id = "sync";
-          inherit devices;
+          devices = builtins.attrNames config.services.syncthing.settings.devices;
         };
       };
 
