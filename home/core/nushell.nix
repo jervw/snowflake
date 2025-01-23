1c7964ab0339fd7cad1c7574886d915137c4d520
jervw <jervw@pm.me>
2025-01-23 03:41:44 +0200

    nu: Add more configuration, cleanup module

--- home/core/nushell.nix
+++ home/core/nushell.nix
@@ -14,9 +14,15 @@
         whereami = "curl ipinfo.io/city";
       };
       extraConfig = ''
-        $env.config.buffer_editor = "hx"
+        # Variables
         $env.EDITOR = "hx"
+
+        # Config
+        $env.config.buffer_editor = "hx"
         $env.config.show_banner = false
+        $env.config.rm.always_trash = true
+        $env.config.highlight_resolved_externals = true
+
 
         # Fish completer
         let fish_completer = {|spans|
@@ -24,23 +30,23 @@
           | from tsv --flexible --noheaders --no-infer
           | rename value description
         }
+
+        # Shorthand for 'nix shell nixpkgs#<package>' to temporarily install a package
+        def nsh [...packages: string] {
+          let pkgs = ($packages | each {|p| $"nixpkgs#($p)"})
+          ^nix shell ...$pkgs
+        }
       '';
       plugins = [pkgs.nushellPlugins.highlight];
     };
 
     # Nix-your-shell
-    nix-your-shell = {
-      enable = true;
-    };
+    nix-your-shell.enable = true;
 
     # Completions
-    carapace = {
-      enable = true;
-    };
+    carapace.enable = true;
 
     # Prompt
-    starship = {
-      enable = true;
-    };
+    starship.enable = true;
   };
 }
