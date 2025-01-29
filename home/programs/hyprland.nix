4abf9a48fb2beba710b3fffbdccbe6675ccfc5cd
jervw <jervw@pm.me>
2025-01-29 18:31:21 +0200

    home: Add Hyprland window rules

--- home/programs/hyprland.nix
+++ home/programs/hyprland.nix
@@ -114,8 +114,11 @@ in {
         "noshadow, floating:0"
         "suppressevent maximize, class:^(zen)$"
         "float, title:^(Volume Control)$"
+        "size 900 500, title:^(Volume Control)$"
         "float, class:mpv"
-        "float, title:^(Picture in picture)$"
+        "float, class:^(zen)$, title:^(Picture-in-Picture)$"
+        "pin, class:^(zen)$, title:^(Picture-in-Picture)$"
+        "size 800 450, class:^(zen)$, title:^(Picture-in-Picture)$"
         "workspace 1, class:(vesktop)"
         "workspace special, class:(cider)"
       ];
