e9d3603021eec65cb9f3941b27c1a5ade909414d
jervw <jervw@pm.me>
2025-01-30 16:47:18 +0200

    loki: Enable Secure boot

--- modules/extra/default.nix
+++ modules/extra/default.nix
@@ -4,7 +4,7 @@ _: {
     ./greetd.nix
     # ./sddm.nix NOTE: Use when using other desktop environments
     ./impermanence.nix
-    # ./lanzaboote.nix
+    ./lanzaboote.nix
     ./stylix.nix
     ./gaming.nix
     ./quiet.nix
