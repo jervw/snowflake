bed433751c9f07c6f5950143fb0de21095363035
jervw <jervw@pm.me>
2025-01-31 00:20:41 +0200

    home: Add missing `user` section for jujutsu

--- home/core/jujutsu.nix
+++ home/core/jujutsu.nix
@@ -2,8 +2,10 @@
   programs.jujutsu = {
     enable = true;
     settings = {
-      name = config.programs.git.userName;
-      email = config.programs.git.userEmail;
+      user = {
+        name = config.programs.git.userName;
+        email = config.programs.git.userEmail;
+      };
     };
   };
 }
