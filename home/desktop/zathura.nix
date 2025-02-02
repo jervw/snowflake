5f66c7c78de6231ea79c33eef7f42578f824d327
jervw <jervw@pm.me>
2025-02-02 19:37:39 +0200

    home/zathura: Additional configuration

--- home/desktop/zathura.nix
+++ home/desktop/zathura.nix
@@ -1,5 +1,15 @@
 _: {
   programs.zathura = {
     enable = true;
+    options = {
+      selection-notification = true;
+      selection-clipboard = "clipboard";
+      adjust-open = "best-fit";
+      pages-per-row = "1";
+      scroll-page-aware = "true";
+      scroll-full-overlap = "0.01";
+      scroll-step = "100";
+      zoom-min = "10";
+    };
   };
 }
