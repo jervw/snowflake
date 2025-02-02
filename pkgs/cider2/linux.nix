d45aa1d9c009d023edc4ee8838c6f86c0634d794
jervw <jervw@pm.me>
2025-02-02 19:45:38 +0200

    pkgs/cider2: Fix missing icon in Desktop entry

--- pkgs/cider2/linux.nix
+++ pkgs/cider2/linux.nix
@@ -20,9 +20,8 @@ in
         --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
         --add-flags '--disable-seccomp-filter-sandbox --ignore-gpu-blocklist --enable-gpu-rasterization --enable-gpu --enable-features=Vulkan,UseSkiaRenderer,VaapiVideoDecoder,CanvasOopRasterization,VaapiVideoEncoder,RawDraw --disable-features=UseChromeOSDirectVideoDecoder --enable-zero-copy --enable-oop-rasterization --enable-raw-draw --enable-accelerated-mjpeg-decode --enable-accelerated-video --enable-native-gpu-memory-buffers'
 
-      # TODO Icons missing from root
-      # install -m 444 -D '${extracted}/usr/share/icons/hicolor/512x512/apps/${pname}.png' \
-      #   "$out/share/icons/hicolor/512x512/apps/${pname}.png"
+      install -m 444 -D '${extracted}/cider.png' \
+        "$out/share/icons/hicolor/512x512/apps/cider.png"
 
       install -m 444 -D '${extracted}/${pname}.desktop' \
         "$out/share/applications/${pname}.desktop"
