9a64baf43789fe414144704e6d91b7768c8d11f9
jervw <jervw@pm.me>
2025-02-13 03:31:21 +0200

    modules/impermamence: Add stuff

--- modules/extra/impermanence.nix
+++ modules/extra/impermanence.nix
@@ -35,7 +35,7 @@ in {
           ".dots"
           ".zen"
         ]
-        ++ forEach ["dconf" "heroic" "obsidian" "rclone" "sh.cider.genten" "nushell" "vesktop" "obs-studio" "github-copilot" "zed" "gh"] (
+        ++ forEach ["dconf" "heroic" "obsidian" "rclone" "sh.cider.genten" "nushell" "vesktop" "obs-studio" "github-copilot" "zed" "gh" "FreeTube" "harper-ls"] (
           x: ".config/${x}"
         )
         ++ forEach ["nix" "zen"] (
