0558502fedff31b2db00ea19eb04d629127064b6
jervw <jervw@pm.me>
2025-02-13 16:48:59 +0200

    modules/caddy: Update caddy-dns/cloudflare hash

--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -8,7 +8,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e"];
-      hash = "sha256-jCcSzenewQiW897GFHF9WAcVkGaS/oUu63crJu7AyyQ=";
+      hash = "sha256-JVkUkDKdat4aALJHQCq1zorJivVCdyBT+7UhqTvaFLw=";
     };
     extraConfig = ''
       (cloudflare) {
