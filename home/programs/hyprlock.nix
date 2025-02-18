c13dd6c0526d58908be3839fc0b2c1ab73dd866b
jervw <jervw@pm.me>
2025-02-18 18:35:27 +0200

    home/hyprlock: Make input field transparent, add date field

--- home/programs/hyprlock.nix
+++ home/programs/hyprlock.nix
@@ -1,4 +1,4 @@
-_: {
+{lib, ...}: {
   programs.hyprlock = {
     enable = true;
 
@@ -17,21 +17,35 @@ _: {
       input-field = {
         outline_thickness = 2;
 
-        fade_on_empty = false;
-        placeholder_text = "Password...";
+        # Override Stylix and make it transparent
+        outer_color = lib.mkForce "rgba(0,0,0,0)";
+        inner_color = lib.mkForce "rgba(0,0,0,0)";
+        check_color = lib.mkForce "rgba(0,0,0,0)";
+
+        placeholder_text = "";
 
         dots_size = 0.33;
         dots_spacing = 0.15;
         dots_center = true;
       };
 
-      label = {
-        text = "$TIME";
-        font_size = 75;
-        position = "0, 200";
-        valign = "center";
-        halign = "center";
-      };
+      label = [
+        {
+          text = "$TIME";
+          font_size = 75;
+          position = "0, 200";
+          valign = "center";
+          halign = "center";
+        }
+        {
+          text = ''cmd[update:1000] echo $(date +"%A, %B %d")'';
+          color = "rgba(242, 243, 244, 0.75)";
+          font_size = 22;
+          position = "0, 100";
+          halign = "center";
+          valign = "center";
+        }
+      ];
     };
   };
 }
