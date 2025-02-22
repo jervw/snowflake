be52889bc524e3b419ecbba42112e78ee3fcf72a
jervw <jervw@pm.me>
2025-02-22 20:15:15 +0200

    modules/redlib: Cleanup and remove default wide layout

--- modules/server/redlib.nix
+++ modules/server/redlib.nix
@@ -10,18 +10,17 @@
 
   services.redlib = {
     enable = true;
+    port = 8081;
     settings = {
-      REDLIB_DEFAULT_WIDE = "on";
       REDLIB_DEFAULT_SHOW_NSFW = "on";
       REDLIB_DEFAULT_USE_HLS = "on";
       REDLIB_DEFAULT_HIDE_HLS_NOTIFICATION = "on";
     };
-    port = 8081;
   };
 
   age.secrets.reddit.file = "${self}/secrets/reddit.age";
 
-  # Used for REDLIB_DEFAULT_SUBSCRIPTIONS, i prefer to keep my subs private
+  # Used for REDLIB_DEFAULT_SUBSCRIPTIONS, I prefer to keep my subs private
   systemd.services.redlib = {
     serviceConfig = {
       EnvironmentFile = [config.age.secrets.reddit.path];
