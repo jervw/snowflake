b813691ae193dda5d1dbdbb8d19bae51e26c6f4c
jervw <jervw@pm.me>
2025-03-10 18:26:50 +0200

    home/niri: Add window rules

--- home/wayland/niri/rules.nix
+++ home/wayland/niri/rules.nix
@@ -1,8 +1,46 @@
-_: {
-  programs.niri.settings.window-rules = [
+{...}: let
+  mkMatchRule = {
+    appId,
+    title ? "",
+    openFloating ? false,
+  }: let
+    baseRule = {
+      matches = [
+        {
+          app-id = appId;
+          inherit title;
+        }
+      ];
+    };
+    floatingRule =
+      if openFloating
+      then {open-floating = true;}
+      else {};
+  in
+    baseRule // floatingRule;
+
+  openFloatingAppIds = [
+    "^(pwvucontrol)"
+    "^(Volume Control)"
+    "^(dialog)"
+    "^(file_progress)"
+    "^(confirm)"
+    "^(download)"
+    "^(error)"
+    "^(notification)"
+  ];
+
+  floatingRules = builtins.map (appId:
+    mkMatchRule {
+      appId = appId;
+      openFloating = true;
+    })
+  openFloatingAppIds;
+
+  windowRules = [
     {
       geometry-corner-radius = let
-        radius = 15.0;
+        radius = 16.0;
       in {
         bottom-left = radius;
         bottom-right = radius;
@@ -12,5 +50,46 @@ _: {
       clip-to-geometry = true;
       draw-border-with-background = false;
     }
+    {
+      matches = [
+        {is-floating = true;}
+      ];
+      shadow.enable = true;
+    }
+    {
+      matches = [
+        {
+          is-window-cast-target = true;
+        }
+      ];
+    }
+    {
+      matches = [
+        {app-id = "^(zen|firefox||zen-.*)$";}
+      ];
+      open-maximized = true;
+    }
+    {
+      matches = [
+        {
+          app-id = "firefox$";
+          title = "^Picture-in-Picture$";
+        }
+        {
+          app-id = "zen-.*$";
+          title = "^Picture-in-Picture$";
+        }
+        {title = "^Picture in picture$";}
+        {title = "^Discord Popout$";}
+      ];
+      open-floating = true;
+      default-floating-position = {
+        x = 32;
+        y = 32;
+        relative-to = "top-right";
+      };
+    }
   ];
+in {
+  programs.niri.settings.window-rules = windowRules ++ floatingRules;
 }
