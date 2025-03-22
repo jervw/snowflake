42d38de2fd4680f17f9d1545a0539e9370a3ef4e
jervw <jervw@pm.me>
2025-03-22 18:31:09 +0200

    flake: Update

--- home/core/nushell.nix
+++ home/core/nushell.nix
@@ -29,7 +29,7 @@
           | rename value description
         }
       '';
-      plugins = [pkgs.nushellPlugins.highlight];
+      # plugins = [pkgs.nushellPlugins.highlight];
     };
 
     # Nix-your-shell
