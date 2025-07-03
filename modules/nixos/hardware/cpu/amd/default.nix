d8702b02c2840591ad8e99dc442858b4a2e7ee97
jervw <jervw@pm.me>
2025-07-03 22:42:40 +0300

    snowfall/hardware: Add CPU modules (Intel/AMD)

---
+++ modules/nixos/hardware/cpu/amd/default.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf;
+
+  cfg = config.${namespace}.hardware.cpu.amd;
+in {
+  options.${namespace}.hardware.cpu.amd = {
+    enable = lib.mkEnableOption "support for AMD cpu";
+  };
+
+  config = mkIf cfg.enable {
+    boot = {
+      blacklistedKernelModules = ["k10temp"];
+      extraModulePackages = [config.boot.kernelPackages.zenpower];
+      kernelModules = [
+        "kvm-amd"
+        "zenpower"
+      ];
+
+      kernelParams = ["amd_pstate=active"];
+    };
+
+    environment.systemPackages = [pkgs.amdctl];
+
+    hardware.cpu.amd.updateMicrocode = true;
+  };
+}
---
+++ modules/nixos/hardware/cpu/intel/default.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf;
+
+  cfg = config.${namespace}.hardware.cpu.intel;
+in {
+  options.${namespace}.hardware.cpu.intel = {
+    enable = lib.mkEnableOption "support for intel cpu";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [intel-gpu-tools];
+
+    hardware.cpu.intel.updateMicrocode = true;
+
+    boot = {
+      kernelModules = ["kvm-intel"];
+
+      kernelParams = [
+        "i915.fastboot=1"
+        "enable_gvt=1"
+      ];
+    };
+  };
+}
