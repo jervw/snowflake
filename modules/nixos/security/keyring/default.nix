78d22b4be1923a490dbea456a5865fd38aecf8d4
jervw <jervw@pm.me>
2025-07-03 20:39:00 +0300

    snowfall/security: Add keyring module

---
+++ modules/nixos/security/keyring/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf;
+
+  cfg = config.${namespace}.security.keyring;
+in {
+  options.${namespace}.security.keyring = {
+    enable = lib.mkEnableOption "gnome keyring";
+  };
+
+  config = mkIf cfg.enable {
+    services.gnome.gnome-keyring.enable = true;
+    programs.seahorse.enable = true;
+  };
+}
