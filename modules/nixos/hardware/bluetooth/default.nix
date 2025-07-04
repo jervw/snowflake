33e346204feec01f99111ff02a8558adbe546bc3
jervw <jervw@pm.me>
2025-07-04 19:23:37 +0300

    snowfall/hardware: Add bluetooth module

---
+++ modules/nixos/hardware/bluetooth/default.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf;
+
+  cfg = config.${namespace}.hardware.bluetooth;
+in {
+  options.${namespace}.hardware.bluetooth = {
+    enable = lib.mkEnableOption "support for extra bluetooth devices";
+    autoConnect = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to automatically connect to paired devices on startup";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    hardware.bluetooth = {
+      enable = true;
+
+      package = pkgs.bluez-experimental;
+      powerOnBoot = false;
+
+      settings = {
+        General = {
+          ControllerMode = "bredr";
+          Experimental = true;
+          JustWorksRepairing = "always";
+          KernelExperimental = true;
+          MultiProfile = "multiple";
+        };
+        Policy = {
+          AutoEnable = cfg.autoConnect;
+          ReconnectAttempts =
+            if cfg.autoConnect
+            then 7
+            else 0;
+          ReconnectIntervals = "1,2,4,8,16,32,64";
+        };
+      };
+    };
+
+    boot.kernelModules = ["btusb"];
+
+    services.blueman = {
+      enable = true;
+    };
+  };
+}
