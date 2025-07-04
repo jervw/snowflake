429c43724d5372d46b0f51aa557664722f86443e
jervw <jervw@pm.me>
2025-07-04 20:24:15 +0300

    snowfall/networking: Add NFS (Tailscale) module

---
+++ modules/nixos/networking/nfs/default.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  namespace,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkIf;
+  cfg = config.${namespace}.networking.nfs;
+in {
+  options.${namespace}.networking.nfs = {
+    enable = lib.mkEnableOption "Enable Tailscale connected NFS storage";
+  };
+
+  config = mkIf (cfg.enable && config.services.tailscale.enable) {
+    environment.systemPackages = [pkgs.nfs-utils];
+    boot.initrd = {
+      supportedFilesystems = ["nfs"];
+      kernelModules = ["nfs"];
+    };
+
+    fileSystems."/mnt/share" = {
+      device = "thor:/mnt/storage/NAS";
+      fsType = "nfs";
+      options = ["rw" "noauto" "x-systemd.automount" "x-systemd.idle-timeout=600"];
+    };
+  };
+}
