6dbead9ff29ef77b6209a02706ff684c2a57e5b3
jervw <jervw@pm.me>
2025-07-16 23:18:41 +0300

    snowfall/programs: Gamemode init

---
+++ modules/nixos/programs/gamemode/default.nix
@@ -0,0 +1,64 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib.${namespace}) mkOpt;
+
+  cfg = config.${namespace}.programs.gamemode;
+
+  defaultStartScript = ''
+    ${lib.getExe' pkgs.libnotify "notify-send"} 'GameMode started'
+  '';
+
+  defaultEndScript = ''
+    ${lib.getExe' pkgs.libnotify "notify-send"} 'GameMode ended'
+  '';
+in {
+  options.${namespace}.programs.gamemode = {
+    enable = lib.mkEnableOption "gamemode";
+    endscript = mkOpt (with lib.types; nullOr str) null "The script to run when disabling gamemode.";
+    startscript = mkOpt (with lib.types; nullOr str) null "The script to run when enabling gamemode.";
+  };
+
+  # TODO: Add Hyprland scripts (to hyprland module somewhere)
+  # TODO Add GPU overclocking when there is a easy way to do it on Wayland + NVIDIA
+
+  config = let
+    startScript =
+      if (cfg.startscript == null)
+      then pkgs.writeShellScript "gamemode-start" defaultStartScript
+      else pkgs.writeShellScript "gamemode-start" cfg.startscript;
+    endScript =
+      if (cfg.endscript == null)
+      then pkgs.writeShellScript "gamemode-end" defaultEndScript
+      else pkgs.writeShellScript "gamemode-end" cfg.endscript;
+  in
+    lib.mkIf cfg.enable {
+      programs.gamemode = {
+        enable = true;
+        enableRenice = true;
+
+        settings = {
+          general = {
+            softrealtime = "auto";
+            renice = 15;
+          };
+
+          custom = {
+            start = startScript.outPath;
+            end = endScript.outPath;
+          };
+        };
+      };
+
+      security.wrappers.gamemode = {
+        owner = "root";
+        group = "root";
+        source = "${lib.getExe' pkgs.gamemode "gamemoderun"}";
+        capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
+      };
+    };
+}
