c95dc95c0f63a02a69e98f625fb04a204686f5e8
jervw <jervw@pm.me>
2025-07-17 15:51:06 +0300

    snowfall/suites: Enable nsync, add kernel optimizations for gaming suite

--- modules/nixos/suites/gaming/default.nix
+++ modules/nixos/suites/gaming/default.nix
@@ -19,7 +19,6 @@ in {
       # Launchers
       lutris
       heroic
-      bottles
       cartridges
 
       # Emulators
@@ -31,11 +30,30 @@ in {
       mangohud
     ];
 
+    # Some platform optimization that Steam Deck also uses
+    boot.kernel.sysctl = {
+      # 20-shed.conf
+      "kernel.sched_cfs_bandwidth_slice_us" = 3000;
+      # 20-net-timeout.conf
+      # This is required due to some games being unable to reuse their TCP ports
+      # if they're killed and restarted quickly - the default timeout is too large.
+      "net.ipv4.tcp_fin_timeout" = 5;
+      # 30-splitlock.conf
+      # Prevents intentional slowdowns in case games experience split locks
+      # This is valid for kernels v6.0+
+      "kernel.split_lock_mitigate" = 0;
+      # 30-vm.conf
+      # USE MAX_INT - MAPCOUNT_ELF_CORE_MARGIN.
+      # see comment in include/linux/mm.h in the kernel tree.
+      "vm.max_map_count" = 2147483642;
+    };
+
     snowflake = {
       programs = {
         steam = mkDefault enabled;
         gamemode = mkDefault enabled;
         gamescope = mkDefault enabled;
+        ntsync = mkDefault enabled;
       };
     };
   };
