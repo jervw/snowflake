a6ae148532e6bfe0867ee73020fbc07a62a15545
jervw <jervw@pm.me>
2025-07-17 22:57:23 +0300

    snowfall/home: eza init

---
+++ modules/home/programs/terminal/tools/eza/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf;
+
+  cfg = config.${namespace}.programs.terminal.tools.eza;
+in {
+  options.${namespace}.programs.terminal.tools.eza = {
+    enable = lib.mkEnableOption "Enable eza";
+  };
+
+  config = mkIf cfg.enable {
+    programs.eza = {
+      enable = true;
+      colors = "always";
+      icons = "always";
+      extraOptions = [
+        "--group-directories-first"
+        "--header"
+      ];
+    };
+  };
+}
