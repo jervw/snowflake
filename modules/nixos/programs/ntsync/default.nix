68555a57c7e68fab87bf151f287d132f2b89859f
jervw <jervw@pm.me>
2025-07-17 15:50:20 +0300

    snowfall/programs: ntsync init

---
+++ modules/nixos/programs/ntsync/default.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf;
+
+  # TODO: Namespace category maybe little off, move somewhere else?
+
+  cfg = config.${namespace}.programs.ntsync;
+in {
+  options.${namespace}.programs.ntsync = {
+    enable = lib.mkEnableOption "Enable ntsync support";
+  };
+
+  config = mkIf cfg.enable {
+    boot.kernelModules = ["ntsync"];
+
+    # make ntsync device accessible
+    services.udev.packages = [
+      (pkgs.writeTextFile {
+        name = "ntsync-udev-rules";
+        text = ''KERNEL=="ntsync", MODE="0660", TAG+="uaccess"'';
+        destination = "/etc/udev/rules.d/70-ntsync.rules";
+      })
+    ];
+
+    assertions = [
+      {
+        assertion = lib.versionAtLeast config.boot.kernelPackages.kernel.version "6.14";
+        message = "Option `${namespace}.programs.ntsync.enable` requires Linux 6.14+.";
+      }
+    ];
+  };
+}
