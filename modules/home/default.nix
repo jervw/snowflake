6d8ef0936ae97623ed5571bb9c4ede538df4974f
jervw <jervw@pm.me>
2025-07-17 21:20:16 +0300

    snowfall/home: Add shell aliases to home.shellAliases, also set home.stateVersion

--- modules/home/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  lib,
-  osConfig ? {},
-  ...
-}: {
-  home.stateVersion = lib.mkDefault (osConfig.system.stateVersion or "24.05");
-}
--- modules/home/user/default.nix
+++ modules/home/user/default.nix
@@ -1,11 +1,44 @@
 {
+  config,
   lib,
+  pkgs,
+  osConfig ? {},
   namespace,
   ...
-}: {
+}: let
+  inherit
+    (lib)
+    types
+    mkIf
+    getExe
+    ;
+  inherit (lib.${namespace}) mkOpt enabled;
+
+  cfg = config.${namespace}.user;
+in {
   options.${namespace}.user = with lib; {
+    enable = mkOpt types.bool false "Whether to configure the user account.";
     name = mkOpt str "jervw" "The name to use for the user account.";
     fullName = mkOpt str "Jere Vuola" "The full name of the user.";
     email = mkOpt str "jervw@pm.me" "The email of the user.";
+  };
+
+  config = mkIf cfg.enable {
+    home = {
+      shellAliases = {
+        y = "${getExe pkgs.yazi}";
+        lg = "${getExe pkgs.lazygit}";
+        ls = "${getExe pkgs.eza} --icons";
+        cd = "${getExe pkgs.zoxide}";
+        tree = "${getExe pkgs.eza} --tree --icons";
+        whereami = "${getExe pkgs.curl} ipinfo.io/city";
+        myip = "${getExe pkgs.curl} ifconfig.me";
+
+        sl = "ls";
+        flake = "nix flake";
+      };
+      home.stateVersion = lib.mkDefault (osConfig.system.stateVersion or "24.05");
+    };
+    programs.home-manager = enabled;
   };
 }
