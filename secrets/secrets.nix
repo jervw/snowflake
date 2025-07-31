1ae30670d6eac0a9c23ac4d218f29bab2cc1a123
jervw <jervw@pm.me>
2025-07-31 22:16:06 +0300

    secrets: Add huginn to secrets, re-key secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,9 @@ let
   loki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB6J9/WuerF3L6FyZvYr4D8ffrAxIW8IPb9Vw39dFfz";
   thor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM/pd5zQYAXuve8QVj6947m/OTfj3O36Czx6/GUpD1BN";
   fenrir = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOAj0+XMWCCv/FyM8Y3qh9yfkXn83l+MI7CKeXCSm2d";
-  systems = [loki thor fenrir];
+  huginn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKsLmSM41UUDV2UgLZiUlCVzDDgailNwPiHzKkXkNmMB";
+
+  systems = [loki thor fenrir huginn];
 in {
   "cloudflare.age".publicKeys = [jervw] ++ systems;
   "tailscale.age".publicKeys = [jervw] ++ systems;
