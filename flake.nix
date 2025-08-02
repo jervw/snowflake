709aa4e56a3ecf55f038689399244b425ef82d49
jervw <jervw@pm.me>
2025-08-02 18:33:14 +0300

    flake: switch to zen-browser twilight

--- flake.nix
+++ flake.nix
@@ -97,7 +97,7 @@
       homes.modules = with inputs; [
         niri.homeModules.niri
         stylix.homeModules.stylix
-        zen-browser.homeModules.beta
+        zen-browser.homeModules.twilight
       ];
     };
 }
