6b3738df99625b43ff34b1a1b870f51183b34d88
jervw <jervw@pm.me>
2025-08-02 18:29:56 +0300

    nixos/locale: Fix isse with virtual console not loading on boot

--- modules/nixos/system/locale/default.nix
+++ modules/nixos/system/locale/default.nix
@@ -4,7 +4,7 @@
   namespace,
   ...
 }: let
-  inherit (lib) mkIf mkForce;
+  inherit (lib) mkIf;
 
   cfg = config.${namespace}.system.locale;
 in {
@@ -13,11 +13,6 @@ in {
   };
 
   config = mkIf cfg.enable {
-    console = {
-      font = "Lat2-Terminus16";
-      keyMap = mkForce "us";
-    };
-
     environment.variables = {
       LOCALE_ARCHIVE = "/run/current-system/sw/lib/locale/locale-archive";
     };
