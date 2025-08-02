b974022723dca2b982dca7dda20efc3cb8d421ae
jervw <jervw@pm.me>
2025-08-03 00:52:01 +0300

    home/niri: Try something to fix the Niri problem

--- modules/home/programs/graphical/wm/niri/default.nix
+++ modules/home/programs/graphical/wm/niri/default.nix
@@ -15,14 +15,17 @@ in {
     enable = mkEnableOption "Enable niri";
   };
 
-  # imports = mkIf cfg.enable (lib.snowfall.fs.get-non-default-nix-files ./.);
+  imports = lib.snowfall.fs.get-non-default-nix-files ./.;
 
   config = mkIf cfg.enable {
     home.packages = [pkgs.xwayland-satellite];
 
     programs.niri = {
-      enable = true;
-      package = inputs.niri.packages.${system}.niri-unstable;
+      enable = cfg.enable;
+      package =
+        if cfg.enable
+        then inputs.niri.packages.${system}.niri-unstable
+        else null;
     };
   };
 }
