51e882e1c18527bbd023e559415738ad6cfb6297
jervw <jervw@pm.me>
2025-08-05 01:58:43 +0300

    modules/services: Glance, Add custom API

--- modules/nixos/services/glance/default.nix
+++ modules/nixos/services/glance/default.nix
@@ -230,6 +230,15 @@ in {
                         }
                       ];
                     }
+                    {
+                      type = "custom-api";
+                      cache = "3h";
+                      url = "https://bible-api.com/data/web/random";
+                      template = ''
+                        <p class="size-h2 color-highlight">{{ .JSON.String "random_verse.book" }} {{ .JSON.String "random_verse.chapter" }}:{{ .JSON.String "random_verse.verse" }}</p>
+                        <p class="size-h4 color-paragraph">{{ .JSON.String "random_verse.text" }}</p>
+                      '';
+                    }
                   ];
                 }
               ];
