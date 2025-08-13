c9ef99be6b244e554856d3f6ee577488441f5aa7
jervw <jervw@pm.me>
2025-08-13 19:12:26 +0300

    systems/fenrir: Remove conflicting tlp over power-profile-daemons

--- systems/x86_64-linux/fenrir/default.nix
+++ systems/x86_64-linux/fenrir/default.nix
@@ -14,16 +14,6 @@ in {
   # Suspend is broken on T2-Macs since Sonoma
   services.logind.lidSwitch = "ignore";
 
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MAX_PERF_ON_BAT = 70;
-    };
-  };
-
   hardware.apple-t2.kernelChannel = "latest";
 
   boot.loader = {
