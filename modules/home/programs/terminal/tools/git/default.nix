ee075eb1b728276280d640c11b0e250d499e2cf8
jervw <jervw@pm.me>
2025-10-26 17:33:59 +0200

    home/git: Move git settings under `programs.git.settings`

--- modules/home/programs/terminal/tools/git/default.nix
+++ modules/home/programs/terminal/tools/git/default.nix
@@ -11,7 +11,7 @@
   cfg = config.${namespace}.programs.terminal.tools.git;
 in {
   options.${namespace}.programs.terminal.tools.git = {
-    enable = mkEnableOption "Enable atuin";
+    enable = mkEnableOption "Enable git";
     userName = mkOpt types.str user.name "The name to configure git with.";
     userEmail = mkOpt types.str user.email "The email to configure git with.";
     signByDefault = mkOpt types.bool true "Whether to sign commits by default.";
@@ -24,25 +24,30 @@ in {
     programs.git = {
       enable = true;
 
-      inherit (cfg) userName userEmail;
+      settings = {
+        user = {
+          name = cfg.userName;
+          email = cfg.userEmail;
+        };
+
+        aliases = {
+          s = "status";
+          last = "log -1 HEAD";
+          lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
+          l = "lg";
+        };
+
+        extraConfig = {
+          init = {defaultBranch = "main";};
+          push.autoSetupRemote = true;
+        };
+      };
 
       ignores = [
         ".direnv/"
         ".env"
         ".envrc"
       ];
-
-      aliases = {
-        s = "status";
-        last = "log -1 HEAD";
-        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
-        l = "lg";
-      };
-
-      extraConfig = {
-        init = {defaultBranch = "main";};
-        push.autoSetupRemote = true;
-      };
 
       lfs = {
         enable = true;
