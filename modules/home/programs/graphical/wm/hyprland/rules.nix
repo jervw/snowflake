888373117ebd35bad82cfe70655543b2d891df10
jervw <jervw@pm.me>
2026-01-09 19:54:48 +0200

    home/hyprland: Update to new window and layer rule syntax

--- modules/home/programs/graphical/wm/hyprland/rules.nix
+++ modules/home/programs/graphical/wm/hyprland/rules.nix
@@ -1,24 +1,36 @@
 _: {
   wayland.windowManager.hyprland.settings = {
     layerrule = [
-      "ignorezero, waybar"
-      "blur, waybar"
-      "noanim, launcher"
-      "ignorezero, launcher"
-      "blur, launcher"
+      # TODO Add some new rules
+      # "noanim, launcher"
+      # "ignorezero, launcher"
+      # "blur, launcher"
     ];
 
-    windowrulev2 = [
-      "noshadow, floating:0"
-      "suppressevent maximize, class:^(zen)$"
-      "float, title:^(Volume Control)$"
-      "size 900 500, title:^(Volume Control)$"
-      "float, class:mpv"
-      "float, class:^(zen)$, title:^(Picture-in-Picture)$"
-      "pin, class:^(zen)$, title:^(Picture-in-Picture)$"
-      "size 800 450, class:^(zen)$, title:^(Picture-in-Picture)$"
-      "workspace 1, class:(vesktop)"
-      "workspace special, class:(Cider)"
+    windowrule = [
+      # Bitwarden Extension
+      "match:title ^(.*Bitwarden Password Manager.*)$, float on"
+
+      # Firefox/Zen
+      "match:title ^(Picture-in-Picture)$, float on"
+      "match:title ^(Picture-in-Picture)$, pin on"
+
+      # MPV
+      "match:class mpv, float on"
+
+      # Idle inhibit
+      "match:class ^(mpv)$, idle_inhibit focus"
+      "match:class ^(zen)$, match:title ^(.*YouTube.*)$, idle_inhibit focus"
+      "match:class ^(zen)$, idle_inhibit fullscreen"
+
+      "match:class Cider, workspace special"
+      "match:class vesktop, workspace 1 silent"
+
+      # Dim stuff
+      "match:class ^(gcr-prompter)$, dim_around on"
+      "match:class ^(xdg-desktop-portal-gtk)$, dim_around on"
+      "match:class ^(zen)$, match:title ^(File Upload)$, dim_around on"
+      "match:class ^(polkit-gnome-authentication-agent-1)$, dim_around on"
     ];
   };
 }
