0f6622441af77dd81671a76ddc4f66a1fb0d8823
jervw <jervw@pm.me>
2026-01-09 20:30:42 +0200

    nixos/nix: Add Garnix substituter

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -58,6 +58,7 @@ in {
         substituters = [
           "https://cache.nixos.org"
           "https://nix-community.cachix.org"
+          "https://cache.garnix.io"
           "https://niri.cachix.org"
           "https://helix.cachix.org"
           "https://attic.xuyh0120.win/lantian"
@@ -66,6 +67,7 @@ in {
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
           "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
           "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
