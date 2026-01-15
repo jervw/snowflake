6e4e31db258c4e9599ba8a2b69b99cbdb846ea12
jervw <jervw@pm.me>
2026-01-15 22:09:02 +0200

    systems/loki: Add TTY resolution workaround

--- systems/x86_64-linux/loki/default.nix
+++ systems/x86_64-linux/loki/default.nix
@@ -10,6 +10,18 @@ in {
 
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore;
 
+  # Set correct resolution to TTY. Maybe NVIDIA BUG?
+  # Downside of this workaround is that now the secondary monitor is at wrong resolution.
+  systemd.services.fix-tty-resolution = {
+    after = ["multi-user.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.fbset}/bin/fbset -xres 2560 -yres 1440";
+      RemainAfterExit = true;
+    };
+  };
+
   snowflake = {
     hardware = {
       cpu.amd = enabled;
