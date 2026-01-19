497bca4ee7166068f5b37ff9c0059568dd98704e
jervw <jervw@pm.me>
2026-01-19 23:36:58 +0200

    nixos/networking: Remove ipv6 stuff from static-ip

--- modules/nixos/networking/static-ip/default.nix
+++ modules/nixos/networking/static-ip/default.nix
@@ -8,9 +8,6 @@
   inherit (lib) mkEnableOption mkIf mkOption types;
   cfg = config.${namespace}.networking.static-ip;
 
-  # Detect if an address is IPv6
-  isIPv6 = addr: builtins.match ".*:.*" addr != null;
-
   parseAddr = addr: let
     parts = lib.strings.splitString "/" addr;
     address = builtins.elemAt parts 0;
@@ -18,33 +15,22 @@
   in {
     inherit address;
     prefixLength = lib.toInt cidr;
-    isIPv6 = isIPv6 address;
   };
 
-  # Parse all IPs and separate by version
   parsedIPs = map parseAddr (
     if builtins.isList cfg.ip
     then cfg.ip
     else [cfg.ip]
   );
-  ipv4Addrs = builtins.filter (ip: !ip.isIPv6) parsedIPs;
-  ipv6Addrs = builtins.filter (ip: ip.isIPv6) parsedIPs;
 
-  # Parse gateways (support list or single string)
-  gatewayList =
+  # Parse gateway (support list or single string)
+  gateway =
     if builtins.isList cfg.gateway
-    then cfg.gateway
-    else [cfg.gateway];
-  ipv4Gateway = builtins.head (builtins.filter (gw: !isIPv6 gw) gatewayList);
-  ipv6Gateway = let
-    v6Gateways = builtins.filter isIPv6 gatewayList;
-  in
-    if builtins.length v6Gateways > 0
-    then builtins.head v6Gateways
-    else null;
+    then builtins.head cfg.gateway
+    else cfg.gateway;
 in {
   options.${namespace}.networking.static-ip = {
-    enable = mkEnableOption "Enable static IP configuration (IPv4 and/or IPv6)";
+    enable = mkEnableOption "Enable static IP configuration (IPv4)";
     adapter = mkOption {
       type = types.str;
       description = "The name of the network adapter (e.g. enp4s0)";
@@ -52,25 +38,21 @@ in {
     ip = mkOption {
       type = types.either types.str (types.listOf types.str);
       description = ''
-        Static IP address(es) with CIDR notation. Supports:
-        - Single IPv4: "10.0.0.3/26"
-        - Single IPv6: "2001:db8::1/64"
-        - Dual-stack: ["10.0.0.3/26" "2001:db8::1/64"]
+        Static IP address(es) with CIDR notation.
+        Example: "10.0.0.3/26" or ["10.0.0.3/26" "10.0.0.4/26"]
       '';
     };
     gateway = mkOption {
       type = types.either types.str (types.listOf types.str);
       description = ''
-        Default gateway IP address(es). Supports:
-        - Single IPv4: "10.0.0.1"
-        - Single IPv6: "2001:db8::1"
-        - Dual-stack: ["10.0.0.1" "2001:db8::1"]
+        Default gateway IP address.
+        Example: "10.0.0.1"
       '';
     };
     dns = mkOption {
       type = types.listOf types.str;
       default = ["1.1.1.1" "8.8.8.8"];
-      description = "List of DNS server IPs (IPv4 and/or IPv6)";
+      description = "List of DNS server IPs";
     };
   };
 
@@ -78,7 +60,6 @@ in {
     networking = {
       networkmanager.enable = lib.mkForce false;
       useDHCP = lib.mkForce false;
-
       hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
 
       interfaces.${cfg.adapter} = {
@@ -87,23 +68,11 @@ in {
             inherit (ip) address;
             inherit (ip) prefixLength;
           })
-          ipv4Addrs;
-
-        ipv6.addresses =
-          map (ip: {
-            inherit (ip) address;
-            inherit (ip) prefixLength;
-          })
-          ipv6Addrs;
+          parsedIPs;
       };
 
-      defaultGateway = lib.mkIf (ipv4Gateway != null) {
-        address = ipv4Gateway;
-        interface = cfg.adapter;
-      };
-
-      defaultGateway6 = lib.mkIf (ipv6Gateway != null) {
-        address = ipv6Gateway;
+      defaultGateway = {
+        address = gateway;
         interface = cfg.adapter;
       };
 
