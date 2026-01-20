145e8795ebf0a7c86d340c6c0d17352601c8d607
jervw <jervw@pm.me>
2026-01-21 01:35:32 +0200

    home/syncthing: Refactor module, update loki id, change folder labels

--- modules/home/services/syncthing/default.nix
+++ modules/home/services/syncthing/default.nix
@@ -4,9 +4,22 @@
   namespace,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf;
-
+  inherit (lib) mkEnableOption mkIf mapAttrs;
   cfg = config.${namespace}.services.syncthing;
+
+  deviceIds = {
+    loki = "YUFTXMD-BC2B7AI-G4QEDF3-H7DY4JU-73WT4J3-6ZRXX4J-U2XHSCT-MX5X5Q5"; # Desktop
+    thor = "J4JVFCR-ND6TGKN-AKRJ3LA-XUYAYLI-RPQBZO4-OWLILPZ-2LFJH5V-5VWNOQM"; # Server
+    fenrir = "DQVGT7E-XUBOX5G-WSAXS5R-TSTBGGF-32RLEVK-F73VZZP-5IMMIEO-E6FAHAT"; # Laptop
+    tyr = "LF4JYTN-K6OF2N4-JTVUKTU-F5W65QQ-KBAZGXU-46HWVEO-P3VYVGY-SXYKMAM"; # Phone
+  };
+
+  allDevices = builtins.attrNames deviceIds;
+
+  mkFolder = path: {
+    inherit path;
+    devices = allDevices;
+  };
 in {
   options.${namespace}.services.syncthing = {
     enable = mkEnableOption "Enable syncthing service";
@@ -17,50 +30,25 @@ in {
       enable = true;
       guiAddress = "0.0.0.0:8384";
       settings = {
-        devices = {
-          # Desktop
-          loki = {
-            id = "KL2VBOX-JSTIUP4-LVUCM7U-OJTGN4L-VJMYKCI-3AY3RAA-MIAN6EW-YCAAUQC";
+        devices =
+          mapAttrs (_name: id: {
+            inherit id;
             autoAcceptFolders = true;
-          };
-          # Server
-          thor = {
-            id = "J4JVFCR-ND6TGKN-AKRJ3LA-XUYAYLI-RPQBZO4-OWLILPZ-2LFJH5V-5VWNOQM";
-            autoAcceptFolders = true;
-          };
-          # Laptop
-          fenrir = {
-            id = "DQVGT7E-XUBOX5G-WSAXS5R-TSTBGGF-32RLEVK-F73VZZP-5IMMIEO-E6FAHAT";
-            autoAcceptFolders = true;
-          };
-          # Phone
-          tyr = {
-            id = "LF4JYTN-K6OF2N4-JTVUKTU-F5W65QQ-KBAZGXU-46HWVEO-P3VYVGY-SXYKMAM";
-          };
-        };
+          })
+          deviceIds;
 
         folders = {
-          # TODO remove sync directory, rename to obsidian and move to docs/obsidian
-          "~/sync" = {
-            id = "sync";
-            devices = builtins.attrNames config.services.syncthing.settings.devices;
-          };
-          "~/docs" = {
-            id = "docs";
-            devices = builtins.attrNames config.services.syncthing.settings.devices;
-          };
-          "~/music" = {
-            id = "music";
-            devices = builtins.attrNames config.services.syncthing.settings.devices;
-          };
-          "~/vids" = {
-            id = "vids";
-            devices = builtins.attrNames config.services.syncthing.settings.devices;
-          };
-          "~/other" = {
-            id = "other";
-            devices = builtins.attrNames config.services.syncthing.settings.devices;
-          };
+          docs = mkFolder "~/docs";
+          music = mkFolder "~/music";
+          pics = mkFolder "~/pics";
+          vids = mkFolder "~/vids";
+          other = mkFolder "~/other";
+        };
+
+        gui = {
+          user = "user";
+          # TODO add actual password, even if offsite access is not possible
+          password = "apina123";
         };
 
         options = {
