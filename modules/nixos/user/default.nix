ea3e333dc969d4a208ff61b09d66a678818b2388
jervw <jervw@pm.me>
2026-01-21 20:54:08 +0200

    nixos/user: Refactor module, add guard against hashedPasswordFile

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -3,28 +3,28 @@
   lib,
   namespace,
   ...
-}:
-with lib;
-with lib.${namespace}; let
+}: let
+  inherit (lib) types mkIf mkMerge mdDoc;
+  inherit (lib.${namespace}) mkOpt;
   inherit (config.${namespace}) user;
 in {
-  options.${namespace}.user = with lib.types; {
-    name = mkOpt str "jervw" "The name to use for the user account.";
-    fullName = mkOpt str "Jere Vuola" "The full name of the user.";
-    email = mkOpt str "jervw@pm.me" "The email of the user.";
+  options.${namespace}.user = {
+    name = mkOpt types.str "jervw" "The name to use for the user account.";
+    fullName = mkOpt types.str "Jere Vuola" "The full name of the user.";
+    email = mkOpt types.str "jervw@pm.me" "The email of the user.";
     initialPassword =
-      mkOpt str "password"
+      mkOpt types.str "password"
       "The initial password to use when the user is first created.";
-    extraGroups = mkOpt (listOf str) [] "Groups for the user to be assigned.";
-    extraOptions = mkOpt attrs {} (mdDoc "Extra options passed to `users.users.<name>`.");
+    extraGroups = mkOpt (types.listOf types.str) [] "Groups for the user to be assigned.";
+    extraOptions = mkOpt types.attrs {} (mdDoc "Extra options passed to `users.users.<name>`.");
   };
 
   config = {
     users.groups.media = {}; # Used for some services on a server system
-    users.users.${user.name} =
+
+    users.users.${user.name} = mkMerge [
       {
         inherit (user) name;
-
         extraGroups =
           [
             "wheel"
@@ -38,12 +38,14 @@ in {
             "nix"
           ]
           ++ user.extraGroups;
-
         group = "users";
-        hashedPasswordFile = "/persist/password";
         isNormalUser = true;
         description = user.fullName;
       }
-      // user.extraOptions;
+      (mkIf config.${namespace}.system.impermanence.enable {
+        hashedPasswordFile = "/persist/password";
+      })
+      user.extraOptions
+    ];
   };
 }
