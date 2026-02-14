88d8ffca99b8a844346048bd3131e8a52d2ffff4
jervw <jervw@pm.me>
2026-02-14 19:08:03 +0200

    home/helix: Remove jk

--- modules/home/programs/editors/helix/default.nix
+++ modules/home/programs/editors/helix/default.nix
@@ -61,9 +61,6 @@ in {
           X = ["extend_line_up" "extend_to_line_bounds"];
           A-x = "extend_line_above";
         };
-        keys.insert = {
-          j = {k = "normal_mode";};
-        };
         keys.normal.space = {
           c = ":lsp-workspace-command";
           o = ":sh gh repo view --web";
