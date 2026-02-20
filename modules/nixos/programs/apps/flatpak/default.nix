ab104cd20c857c97a1406545c7c671db22cc7342
jervw <jervw@pm.me>
2026-02-20 19:17:54 +0200

    nixos/flatpak: Inline services.flatpak because of xdg.portal dependency

--- modules/nixos/programs/apps/flatpak/default.nix
+++ modules/nixos/programs/apps/flatpak/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   namespace,
   ...
 }: let
@@ -10,12 +11,23 @@ in {
   options.${namespace}.programs.apps.flatpak = {
     enable = lib.mkEnableOption "Enable Flatpak";
   };
-
   config = mkIf cfg.enable {
-    services.flatpak.enable = true;
-    xdg.portal = {
-      enable = true;
-      config.common.default = "*";
+    # Inlined from services.flatpak to avoid forcing xdg.portal.enable on NixOS side.
+    environment.systemPackages = [pkgs.flatpak pkgs.fuse3];
+    security.polkit.enable = true;
+    fonts.fontDir.enable = true;
+    services.dbus.packages = [pkgs.flatpak];
+    systemd.packages = [pkgs.flatpak];
+    systemd.tmpfiles.packages = [pkgs.flatpak];
+    environment.profiles = [
+      "$HOME/.local/share/flatpak/exports"
+      "/var/lib/flatpak/exports"
+    ];
+    users.users.flatpak = {
+      description = "Flatpak system helper";
+      group = "flatpak";
+      isSystemUser = true;
     };
+    users.groups.flatpak = {};
   };
 }
