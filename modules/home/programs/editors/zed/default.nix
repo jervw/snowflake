14a1300bf92a3c900a0a5e3e27a5ef0b26ed6ab7
jervw <jervw@pm.me>
2026-02-25 18:39:06 +0200

    home/zed: Rebuilt configuration

--- modules/home/programs/editors/zed/default.nix
+++ modules/home/programs/editors/zed/default.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  inputs,
   namespace,
   ...
 }: let
@@ -19,37 +20,105 @@ in {
       extraPackages = with pkgs; [
         direnv
       ];
+
+      # Don't allow changes in Zed editor
+      mutableUserSettings = false;
+      # mutableUserKeymaps = false;
+      # mutableUserTasks = false;
+      # mutableUserDebug = false;
+
+      extensions = [
+        # Languages
+        "toml"
+        "nix"
+        "python-requirements"
+        "git-firefly"
+
+        # Themes and icons
+        "colored-zed-icons-theme"
+
+        # Snippets
+        "rust-snippets"
+      ];
+
       userSettings = {
+        # Appearance
+        theme = {
+          mode = "system";
+          light = "Noctalia Light";
+          dark = "Noctalia Dark";
+        };
+        icon_theme = {
+          mode = "system";
+          light = "Colored Zed Icons Theme Light";
+          dark = "Colored Zed Icons Theme Dark";
+        };
+        ui_font_size = 16;
+        buffer_font_size = 15;
+        buffer_font_family = "JetBrainsMono Nerd Font";
+
+        helix_mode = true;
+        base_keymap = "VSCode";
+        relative_line_numbers = "enabled";
+        autosave = "on_focus_change";
+        hour_format = "hour24";
+        auto_update = false;
+        on_last_window_closed = "quit_app";
+        which_key.enabled = true;
+        diagnostics.inline.enabled = true;
+        git.inline_blame.delay_ms = 1000;
+        session.trust_all_worktrees = true;
+        load_direnv = "shell_hook";
+
+        # Disable collab button from dock
+        collaboration_panel.button = false;
+
+        # Agent
         agent = {
           play_sound_when_agent_done = true;
           default_model = {
-            model = "qwen2.5-coder:7b";
             provider = "ollama";
+            model = "qwen2.5-coder:7b";
           };
         };
-        helix_mode = true;
-        relative_line_numbers = true;
 
-        hour_format = "hour24";
-        auto_update = false;
-
+        # Node
         node = {
           path = lib.getExe pkgs.nodejs;
           npm_path = lib.getExe' pkgs.nodejs "npm";
         };
 
-        lsp = {
-          rust-analyzer = {
-            binary = {
-              path = lib.getExe pkgs.rust-analyzer;
-              path_lookup = true;
+        # Disable telemetry
+        telemetry = {
+          diagnostics = false;
+          metrics = false;
+        };
+
+        languages = {
+          Nix = {
+            language_servers = ["nixd" "!nil"]; # Disable nil
+            formatter.external = {
+              command = lib.getExe pkgs.alejandra;
+              arguments = ["--quiet" "--"];
             };
           };
+        };
 
-          nix = {
-            binary = {
-              path_lookup = true;
-            };
+        # MCP
+        context_servers = {
+          mcp-nixos = {
+            command = lib.getExe pkgs.mcp-nixos;
+            args = [];
+          };
+        };
+
+        # LSP
+        lsp = {
+          rust-analyzer = {
+            binary.path = lib.getExe pkgs.rust-analyzer;
+          };
+          nixd = {
+            binary.path = lib.getExe pkgs.nixd;
           };
         };
       };
