10ddf118fa11fee8a5f1f566c2523d7f3a45a524
jervw <jervw@pm.me>
2026-07-01 19:53:40 +0300

    flake: Switch back to unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    home-manager.url = "github:nix-community/home-manager/release-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     impermanence.url = "github:nix-community/impermanence";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     treefmt-nix.url = "github:numtide/treefmt-nix";
