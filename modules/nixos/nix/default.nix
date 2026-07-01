7d035050890e44d4440d28ed585e16be17fcff2a
jervw <jervw@pm.me>
2026-07-01 20:01:21 +0300

    nixos/nix: Remove Garnix substituter

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -49,7 +49,6 @@ in {
         substituters = [
           "https://cache.nixos.org"
           "https://nix-community.cachix.org"
-          "https://cache.garnix.io" # Garnix
           "https://niri.cachix.org" # Niri
           "https://attic.xuyh0120.win/lantian" # CachyOS kernels
           "https://cache.soopy.moe" # Linux T2
@@ -58,7 +57,6 @@ in {
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
           "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
           "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo="
