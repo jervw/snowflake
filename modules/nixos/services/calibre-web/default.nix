a3af8fe6e111b81d6b5bf4aedd82a5b817fe0ff0
jervw <jervw@pm.me>
2026-07-28 17:56:40 +0300

    nixos/calibre-web: init, disabled for now

---
+++ modules/nixos/services/calibre-web/default.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf mkOption;
+  cfg = config.${namespace}.services.calibre-web;
+in {
+  options.${namespace}.services.calibre-web= {
+    enable = mkEnableOption "Enable Calibre-web service";
+    host = mkOption {
+      type = lib.types.str;
+      default = "calibre.jervw.dev";
+      description = "Reverse proxy host name for the Calibre-web service";
+    };
+    port = mkOption {
+      type = lib.types.number;
+      default = 8083;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services = {
+      calibre-web= {
+        enable = false; # TODO enable after build failure fixed
+        openFirewall = true;
+
+        listen = {
+          ip = "0.0.0.0";
+          port = cfg.port;
+        };
+
+        options = {
+          calibreLibrary = "/mnt/storage/Media/Books/Books-Jervw";
+          enableBookUploading = true;
+          enableKepubify = true;
+        };
+      };
+      caddy.virtualHosts."${cfg.host}".extraConfig = ''
+        reverse_proxy http://thor:${toString cfg.port}
+        import cloudflare
+      '';
+    };
+  };
+}
